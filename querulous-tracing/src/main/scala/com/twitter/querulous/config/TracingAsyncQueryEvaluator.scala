04ecf26cd915e007328bd7fd4e00797ae110944c
Johan Oskarsson <johan@oskarsson.nu>
2012-02-03 14:31:55 -0800

    Turn on annotations by default

--- querulous-tracing/src/main/scala/com/twitter/querulous/config/TracingAsyncQueryEvaluator.scala
+++ querulous-tracing/src/main/scala/com/twitter/querulous/config/TracingAsyncQueryEvaluator.scala
@@ -8,7 +8,7 @@ class TracingAsyncQueryEvaluator extends AsyncQueryEvaluator {
 
   var tracerFactory: tracing.Tracer.Factory = tracing.NullTracer.factory
   var serviceName: String = ""
-  var annotateQuery: Boolean = false
+  var annotateQuery: Boolean = true // send info such as service name, ip and trace id with query
 
   protected override def newQueryFactory(stats: querulous.StatsCollector) = {
     new TracingQueryFactory(super.newQueryFactory(stats), serviceName, tracerFactory(), annotateQuery)
