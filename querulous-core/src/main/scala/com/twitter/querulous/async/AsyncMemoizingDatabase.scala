c08e3615f01749bbbd15d4e44ec300fa7c249bb9
Raghavendra Prabhu <rvp@twitter.com>
2012-03-14 13:11:17 -0700

    Add AsyncMemoizingDatabase

---
+++ querulous-core/src/main/scala/com/twitter/querulous/async/AsyncMemoizingDatabase.scala
@@ -0,0 +1,19 @@
+package com.twitter.querulous.async
+
+import scala.collection.mutable
+
+/**
+ *  Like MemoizingDatabaseFactory, but meant for async querulous.
+ */
+class AsyncMemoizingDatabaseFactory(val databaseFactory: AsyncDatabaseFactory) extends AsyncDatabaseFactory {
+  private val databases = new mutable.HashMap[String, AsyncDatabase] with mutable.SynchronizedMap[String, AsyncDatabase]
+
+  def apply(dbhosts: List[String], dbname: String, username: String, password: String, urlOptions: Map[String, String], driverName: String) = synchronized {
+    databases.getOrElseUpdate(
+      dbhosts.toString + "/" + dbname,
+      databaseFactory(dbhosts, dbname, username, password, urlOptions, driverName))
+  }
+
+  // cannot memoize a connection without specifying a database
+  override def apply(dbhosts: List[String], username: String, password: String) = databaseFactory(dbhosts, username, password)
+}
