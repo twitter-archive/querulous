13a18a7cab97155066fbeab71e9b01f8a33477c3
Raghavendra Prabhu <rvp@twitter.com>
2012-03-21 15:50:00 -0700

    Rewire async querulous to a much simpler model, eliminating the checkout pool, thereby saving on two context switches per request

--- querulous-core/src/main/scala/com/twitter/querulous/async/BlockingDatabaseWrapper.scala
+++ querulous-core/src/main/scala/com/twitter/querulous/async/BlockingDatabaseWrapper.scala
@@ -11,8 +11,7 @@ import com.twitter.querulous.database.{Database, DatabaseFactory}
 import com.twitter.querulous.config
 
 class BlockingDatabaseWrapperFactory(
-  workPool: config.FuturePool,
-  checkoutPool: config.FuturePool,
+  workPoolSize: Int,
   factory: DatabaseFactory,
   stats: StatsCollector = NullStatsCollector)
 extends AsyncDatabaseFactory {
@@ -25,107 +24,31 @@ extends AsyncDatabaseFactory {
     driverName: String
   ): AsyncDatabase = {
     new BlockingDatabaseWrapper(
-      workPool(),
-      checkoutPool(),
+      workPoolSize,
       factory(hosts, name, username, password, urlOptions, driverName),
       stats
     )
   }
 }
 
-private object AsyncConnectionCheckout {
-  lazy val checkoutTimer = new JavaTimer(true)
-}
-
 class BlockingDatabaseWrapper(
-  workPool: FuturePool,
-  checkoutPool: FuturePool,
+  workPoolSize: Int,
   protected[async] val database: Database,
   stats: StatsCollector = NullStatsCollector)
 extends AsyncDatabase {
+  private val executor = Executors.newFixedThreadPool(workPoolSize, new DaemonThreadFactory("asyncWorkPool"))
+  private val workPool = FuturePool(executor)
 
-  import AsyncConnectionCheckout._
-
-  getExecutor(workPool) foreach { e =>
-    stats.addGauge("db-async-active-threads")(e.getActiveCount.toDouble)
-  }
-
-  getExecutor(checkoutPool) foreach { e =>
-    val q = e.getQueue
-    stats.addGauge("db-async-waiters")(q.size.toDouble)
-  }
-
-  private val openTimeout  = database.openTimeout
-
-  def withConnection[R](f: Connection => R) = {
-    checkoutConnection() flatMap { conn =>
-      workPool {
-        f(conn)
-      } ensure {
-        closeConnection(conn)
-      }
+  def withConnection[R](f: Connection => R): Future[R] = {
+    workPool {
+      database.withConnection(f)
     }
   }
 
-  private def checkoutConnection(): Future[Connection] = {
-
-    // XXX: there is a bug in FuturePool before util 1.12.5 that
-    // causes it to potentially drop completed work when cancelled. As
-    // a workaround, we create a promise explicitly and set it from
-    // the future pool in order to prevent the checkout FuturePool
-    // from receiving cancellation signals.
-    val conn = new Promise[Connection]()
-
-    stats.timeFutureMillis("db-async-open-timing") {
-      checkoutPool { conn() = Try(database.open()) }
-    }
-
-    // Return within a specified timeout. If within times out, that
-    // means the connection is never handed off, so we need to clean
-    // up ourselves.
-    conn.within(checkoutTimer, openTimeout) onFailure {
-      case e: java.util.concurrent.RejectedExecutionException => {
-        stats.incr("db-async-open-rejected-count", 1)
-      }
-      case e: java.util.concurrent.TimeoutException => {
-        stats.incr("db-async-open-timeout-count", 1)
-
-        // Cancel the checkout.
-        conn.cancel()
-
-        // If the future is not cancelled in time, this will close the
-        // dangling connection.
-        conn foreach { closeConnection(_) }
-      }
-      case _ => {}
-    }
-  }
-
-  private def closeConnection(c: Connection) {
-    try {
-      database.close(c)
-    } catch {
-      case e => {
-        val l = Logger.getLogger("querulous")
-        l.log(Level.WARNING, "Exception on database close.", e)
-      }
-    }
-  }
-
-  private def getExecutor(p: FuturePool) = p match {
-    case p: ExecutorServiceFuturePool => p.executor match {
-      case e: java.util.concurrent.ThreadPoolExecutor => Some(e)
-      case _ => None
-    }
-    case _ => None
-  }
-
-  // equality overrides
-
+  // Equality overrides.
   override def equals(other: Any) = other match {
     case other: BlockingDatabaseWrapper => database eq other.database
     case _ => false
   }
-
   override def hashCode = database.hashCode
 }
