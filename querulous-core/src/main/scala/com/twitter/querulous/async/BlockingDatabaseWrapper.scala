3a667c296b676f8bddffcdf9a614c224d8cfe2ea
Raghavendra Prabhu <rvp@twitter.com>
2012-03-22 15:11:27 -0700

    Implement open timeout enforcement in the new async querulous

--- querulous-core/src/main/scala/com/twitter/querulous/async/BlockingDatabaseWrapper.scala
+++ querulous-core/src/main/scala/com/twitter/querulous/async/BlockingDatabaseWrapper.scala
@@ -3,14 +3,15 @@ package com.twitter.querulous.async
 import java.util.logging.{Logger, Level}
 import java.util.concurrent.Executors
 import java.sql.Connection
-import com.twitter.util.{Future, FuturePool}
+import com.twitter.util.{Future, FuturePool, JavaTimer, TimeoutException}
 import com.twitter.querulous.{StatsCollector, NullStatsCollector, DaemonThreadFactory}
 import com.twitter.querulous.database.{Database, DatabaseFactory}
 import com.twitter.querulous.config
 
 class BlockingDatabaseWrapperFactory(
   workPoolSize: Int,
-  factory: DatabaseFactory)
+  factory: DatabaseFactory,
+  stats: StatsCollector = NullStatsCollector)
 extends AsyncDatabaseFactory {
   def apply(
     hosts: List[String],
@@ -22,22 +23,47 @@ extends AsyncDatabaseFactory {
   ): AsyncDatabase = {
     new BlockingDatabaseWrapper(
       workPoolSize,
-      factory(hosts, name, username, password, urlOptions, driverName)
+      factory(hosts, name, username, password, urlOptions, driverName),
+      stats
     )
   }
 }
 
+private object AsyncConnectionCheckout {
+  lazy val checkoutTimer = new JavaTimer(true)
+}
+
 class BlockingDatabaseWrapper(
   workPoolSize: Int,
-  protected[async] val database: Database)
+  protected[async] val database: Database,
+  stats: StatsCollector = NullStatsCollector)
 extends AsyncDatabase {
-  private val executor =
-      Executors.newFixedThreadPool(workPoolSize, new DaemonThreadFactory("asyncWorkPool-" + database.hosts.mkString(",")))
-  private val workPool = FuturePool(executor)
+  import AsyncConnectionCheckout._
+
+  private val workPool = FuturePool(Executors.newFixedThreadPool(
+      workPoolSize, new DaemonThreadFactory("asyncWorkPool-" + database.hosts.mkString(","))))
+  private val openTimeout = database.openTimeout
 
   def withConnection[R](f: Connection => R): Future[R] = {
-    workPool {
+    val future = workPool {
       database.withConnection(f)
+    }
+
+    // We need to ensure open timeout is enforced correctly. If a thread is available immediately in workPool,
+    // most likely a connection is available too, but if not, the underlying database is responsible for timing
+    // out the connection open() call. But if a thread is *not* immediately available, we don't want our
+    // request hanging there indefinitely, hence we use the mechanism below.
+    future.within(checkoutTimer, openTimeout) onFailure {
+      case e: TimeoutException => {
+        stats.incr("db-async-open-timeout-count", 1)
+        // Cancel the future. Note that the ExecutorServiceFuturePool only actually propagates the cancel
+        // if the task hasn't run yet, which is exactly the behavior we want. Our goal is to abort requests
+        // waiting too long for an available connection, not those that already got one and are already
+        // executing (there is a separate query timeout mechanism for that).
+        future.cancel()
+      }
+
+      case _ => {}
     }
   }
 
